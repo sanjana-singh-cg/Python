19634f89ac8c6e9476127a727cd6e8cb393d7bd6
sanjana-singh-cg <sanjana.singh.cg@gmail.com>
2026-09-02 16:32:20 +0530

    file modified

---
+++ python01/python string.py
@@ -0,0 +1,64 @@
+# string
+text="python"
+print(text[0])
+print(text[3])
+print(text[-1])
+print(text[-2])
+
+text="programming"
+print(text[0:4])
+print(text[3:8])
+print(text[:5])
+print(text[5:])
+
+text="python"
+print(text[::2])
+print(text[1::2])
+print(text[::-1])
+
+text="Hello World"
+print(len(text))
+print(text[5])
+print(text[-1])
+
+name="sanjana singh"
+city='Patna'
+favorite_programming_language="python"
+message='hi everyone'
+print(name,city,favorite_programming_language,message)
+
+text=""
+print(text,(type)(text),len(text))
+
+text="python programming"
+print(len(text))
+print(text)
+print(text[0])
+print(text[5])
+print(text[2])
+print(text[17])
+
+text="programming"
+print(text[0])
+print(text[1])
+print(text[4])
+print(text[10])
+
+print(text[-1])
+print(text[-2])
+print(text[-3])
+print(text[-11])
+
+name="sanjana singh"
+print(name[0])
+print(name[7])
+print(name[9])
+
+text="python programming"
+print(text[0:6])
+print(text[7:11])
+print(text[0:11])
+print(text[0:5])
+print(text[::2])
+text="python"
+print(text[0:0:-1])
