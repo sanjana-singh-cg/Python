48a1929b58226f32020160cb95c9079d7d69b888
sanjana-singh-cg <sanjana.singh.cg@gmail.com>
2026-09-08 09:31:43 +0530

    file updated

--- python01/condition.py
+++ python01/condition.py
@@ -22,44 +22,148 @@
  #   print("you're pass")
 
 
-number=int(input("enter your number: ").split()[0])
+#number=int(input("enter your number: ").split()[0])
 
-if number%2==0:
-    print("positive")
+#if number%2==0:
+ #   print("positive")
 
-else:
-    print("not positive")
+#else:
+#    print("not positive")
+
+#age=int(input("enter your age: ").split()[0])
+
+#if age>=18:
+   # print("Adult")
+
+#else:
+ #   print("Minor")
+
+#Number=int(input("enter your number: ").split()[0])
+
+#if Number%2==0:
+ #   print("The number is even")
+
+#else:
+ #   print("The number is odd")
+
+
+#percentage=int(input('enter your percentage: ').split()[0])
+
+#if percentage>=40:
+ #   print("you are pass")
+
+#else:
+ #   print("you are fail")
+
+#greater=int(input("enter your first number: ").split()[0])
+#smaller=int(input("enter your second number: ").split()[0])
+
+#if greater>smaller:
+   # print("number is greater")
+
+#telse:
+    #print("number is smaller")
+
+#marks=int(input("enter your marks: ").split()[0])
+
+#if marks>=90:
+ #   print(f"your grade is: A")
+
+#elif 75<=marks<=89:
+ #   print(f"your grade is: B")
+
+#elif 60<=marks<=74:
+ #   print(f"your grade is: C")
+
+#elif 40<=marks<=59:
+ #   print(f"your grade is: D")
+
+#elif marks<=40:
+ #   print(f"your grade is: F")
+
+
+#number=int(input("enter you number:" ).split()[0])
+#if number>0:
+ #   print("The number is positive")
+#elif number<0:
+ #   print("the number is negative")
+#elif number==0:
+ #   print("the nuumber is zero")
+
+#days=int(input("enter a number according to this list:  ").split()[0])
+#if days==1:
+ #   print("Monday")
+#elif days==2:
+ #   print("Tuesday")
+#elif days==3:
+ #   print("Wednesday")
+#elif days==4:
+#    print("Thursday")
+#elif days==5:
+ #   print("Friday")
+#else:
+  #  print("please enter a valid number!!" )
+
+#marks=int(input("enter your marks: ").split()[0])
+#if 80<=marks<=100:
+ #   print("Excellent")
+#elif 70<=marks<80:
+ #   print("Good")
+#elif 40<=marks<70:
+ #   print("Pass")
+#elif marks<40:
+ #   print("Fail")
+
+#number=int(input("enter your number").split()[0])
+#if number==1:
+
+#marks=int(input("enter yor marks: ").split()[0])
+#if marks==75:
+ #   print("Good")
+#elif marks>=40:
+ #   print("you are passed")
+#else:
+  #  print("you are failed")
+
+#number=int(input("enter your number: ").split()[0])
+#if number>=0:
+ #   if number>=100:
+  #      print("number is greater than 100")
+        
+    
+#else:
+ #   print("number is not greater than 100")
 
 age=int(input("enter your age: ").split()[0])
-
-if age>=18:
-    print("Adult")
-
+marks=int(input("enter your marks: ").split()[0])
+if age>=18 and marks>=40:
+    print("Eligible")
 else:
-    print("Minor")
+    print("try again please!!")
 
-Number=int(input("enter your number: ").split()[0])
-
-if Number%2==0:
-    print("The number is even")
-
+number=int(input("enter your number: ").split()[0])
+if number<10 or number>100:
+    print("special")
 else:
-    print("The number is odd")
+    print("try again!!")
 
-
-percentage=int(input('enter your percentage: ').split()[0])
-
-if percentage>=40:
-    print("you are pass")
-
+id=True
+age=int(input("enter your age: ").split()[0])
+if age>=18 and id==True:
+    print("you are allowed")
 else:
-    print("you are fail")
+    print("you are not allowed")
 
-greater=int(input("enter your first number: ").split()[0])
-smaller=int(input("enter your second number: ").split()[0])
-
-if greater>smaller:
-    print("number is greater")
-
+number1=int(input("enter your first number: ").split()[0])
+number2=int(input("enter your second number: ").split()[0])
+if number1>10 and number2>10:
+    print("Both are greater than 10")
 else:
-    print("number is smaller")
+    print("not valid!!")
+
+first_number=int(input("enter your 1 number: ").split()[0])
+if first_number<0 or first_number>100:
+    print("valid")
+else:
+    print("not valid!!")
+    
