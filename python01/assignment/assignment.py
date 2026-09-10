958e80ebb384f977f06735ba8aa7507d5ada77d4
sanjana-singh-cg <sanjana.singh.cg@gmail.com>
2026-09-10 09:33:24 +0530

    assignment

---
+++ python01/assignment/assignment.py
@@ -0,0 +1,262 @@
+#number=int(input("enter your number: ").split()[0])
+#if number>0:
+ #   print("positive")
+#elif number<0:
+ #   print("negative")
+#elif number==0:
+  #  print("zero")
+
+#number=int(input("enter your number: ").split()[0])
+#if number>0 and number%2==0:
+ #   if number%2==0:
+  #      print("even")
+  #  print("positive even")
+#elif number>0:
+  #  if number%2==1:
+  #      print("odd")
+  #  print("positive odd")
+#number1=int(input("enter your first number: ").split()[0])
+#number2=int(input("enter your second number: ").split()[0])
+#if number1>number2:
+ #   print(f"largest number:{number1}")
+#elif number2>number1:
+  #  print(f"largest number:{number2}")
+#elif number1==number2:
+   # print("both are equal")
+
+#number1=int(input("enter your first number: ").split()[0])
+#number2=int(input("enter your second number: ").split()[0])
+#number3=int(input("enter your third number: ").split()[0])
+#if number1>number2 and number3>number2:
+ #   print(f"smallest number:{number2}")
+#elif number2>number1 and number3>number1:
+ #   print(f"smallest number:{number1}")
+#elif number1>number3 and number2>number3:
+    #print(f"smallest number:{number3}")
+
+#number1=int(input("enter your first number: ").split()[0])
+#number2=int(input("enter your second number: ").split()[0])
+#number3=int(input("enter your third number: ").split()[0])
+#if number1>number2 and number1>number3:
+ #   print(f"largest number:{number1}")
+#elif number2>number1 and number2>number3:
+ #   print(f"largest number:{number2}")
+#elif number3>number1 and number3>number2:
+   # print(f"largest number:{number3}")
+
+#number1=int(input("enter your first number: ").split()[0])
+
+#if number1%5==0 and number1%11==0:
+ #   print("divisible by both 5 and 11")
+#elif number1%5==0:
+ #   print("divisible by only 5")
+#elif number1%11==0:
+ #   print("divisible by only 11")
+#else:
+  #  print("divisible by neither")
+
+#number1=int(input("enter your first number: ").split()[0])
+
+#if number1%3==0 and number1%7==0:
+ #   print("divisible by both 3 and 7")
+#elif number1%3==0:
+ #   print("divisible by only 3")
+#elif number1%7==0:
+ #   print("divisible by only 7")
+#else:
+   #  print("divisible by neither")
+
+#marks=int(input("enter your marks: ").split()[0])
+
+#if marks<0:
+ #   print("invalid marks")
+#elif marks>100:
+ #   print("invalid marks")
+#elif marks>=40:
+ #   print("you are pass")
+#elif marks<40:
+ #   print("you are fail")
+
+#marks=int(input("enter your marks: ").split()[0])
+
+#if 90<=marks<=100:
+ #   print("Grade: A")
+#elif 80<=marks<=89:
+ #   print("Grade B")
+#elif number1%11==0:
+ #   print("divisible by only 11")
+#else:
+  #  print("divisible by neither")
+
+#marks=int(input("enter your marks: ").split()[0])
+#if 90<=marks<=100:
+ #   print("Grade: A")
+#elif 80<=marks<=89:
+ #   print("Grade: B")
+#elif 70<=marks<=79:
+ #   print("Grade: C")
+#elif 60<=marks<=69:
+ #   print("Grade: D")
+#elif 40<=marks<=59:
+ #   print("Grade: E")
+#elif marks<40:
+ #   print("You are fail")
+#else:
+ #   print("Please enter valid marks!!")
+
+
+#age=int(input("enter your age: ").split()[0])
+#if 18<=age<=100:
+ #   print("You can vote")
+#elif age>=100:
+ #   print("Rejected!!")
+#elif 0<=age<=18:
+ #  print("You cannot vote")
+#elif age<0:
+   # print("Invalid age!!")
+
+#year=int(input("enter your year: ").split()[0])
+#if year%4==0:
+ #   print("Leap year") 
+#elif year%4!=0:
+   # print("Not a leap year")
+
+# character=input("enter your alphabet: ").split()[0].lower()
+# if character in "aeiou" :
+#     print("Vowel")
+# elif character!="aeiou":
+#     print("Consonant")
+# else:
+#     print("Invalid input")
+
+# cost_price=int(input("enter your cost price: ").split()[0])
+# selling_price=int(input("enter your sell price: ").split()[0])
+# if selling_price>cost_price:
+#     print(f"profit is: {selling_price-cost_price}")
+# elif cost_price>selling_price:
+#     print(f"loss is: {cost_price-selling_price}")
+# elif selling_price==cost_price:
+#     print("no profit and no loss")
+
+# cost_price=int(input("enter your cost price: ").split()[0])
+# selling_price=int(input("enter your sell price: ").split()[0])
+# if selling_price>cost_price:
+#      print(f"profit is: {selling_price-cost_price}")
+#      print("percentage: ")
+# elif cost_price>selling_price:
+#      print(f"loss is: {cost_price-selling_price}")
+# elif selling_price==cost_price:
+    #
+    #  print("no profit and no loss") 
+
+elec_bill=int(input("enter your unit: ").split()[0])
+if elec_bill>=100:
+     print()
+
+
+
+
+# num1=int(input("enter your first number:" ).split()[0])   
+# num2=int(input("enter your second number:" ).split()[0])
+# operator=int(input("enter your first number:\n1.+\n2.-\n3.*\n4./" ).split()[0])
+# if operator==1:
+#      print(f"Add: {num1+num2}")
+# elif operator==2:
+#      print(f"Sub: {num1-num2}")
+# elif operator==3:
+#      print(f"Multi: {num1*num2}")
+# elif operator==4:
+#      print(f"div: {num1/num2}")
+
+# temp=int(input("enter your temp: ").split()[0])
+# if temp<0:
+#      print("freezing")
+# elif 0<=temp<=15:
+#      print("very cold")
+# elif 16<=temp<=25:
+#      print("cold")
+# elif 26<=temp<=35:
+#      print("normal")
+# elif temp>=35:
+#      print("hot")
+
+# num=int(input("enter your num: ").split()[0])
+# if num<0:
+#      print("negative value")
+# elif 0<=num<=10:
+#      print("number is between 0 and 10")
+# elif 11<=num<=50:
+#      print("number is between 11 and 50")
+# elif 51<=num<=100:
+#      print("number is between 51 and 100")
+# elif num>100:
+#      print("number is greater tha 100")
+
+# side1=int(input("enter your first side: ").split()[0])
+# side2=int(input("enter your second side: ").split()[0])
+# side3=int(input("enter your third side: ").split()[0])
+# if side1+side2>side3:
+#      print("valid triangle")
+# elif side1+side3>side2:
+#      print("valid triangle")
+# elif side2+side3>side1:
+#      print("valid triangle")
+# else:
+#      print("invalid triangle")
+
+# side1=int(input("enter your first side: ").split()[0])
+# side2=int(input("enter your second side: ").split()[0])
+# side3=int(input("enter your third side: ").split()[0])
+# if side1==side2==side3:
+#      print("equilateral triangle")
+# elif side1==side2 or side2==side3 or side1==side3:
+#      print("isosceles triangle")
+# elif side1!=side2!=side3:
+#      print("scalene triangle")
+
+# acc=int(input("enter your account balance: ").split()[0])
+# withd=int(input("enter your withdraw amount: ").split()[0])
+# if withd>0 and withd%100==0 and acc>withd:
+#      print("withdrawal successful")
+#      if acc-withd>=500 :
+#          print(f"remaining balance: {acc-withd}")
+
+# username=input("enter your username: ").split()
+# password=int(input("enter your password: ").split()[0])
+# if username=="sanjana-singh":
+#      if password==123456:
+#           print("Login successful")
+
+# elif username=="sanjana-singh":
+#      if password!=123456:
+#           print("wrong password")
+
+# elif username!="sanjana-singh":
+#      if password==123456:
+#           print("user not found")
+
+# student_age=int(input("enter your age: ").split()[0])
+# marks=int(input("enter your marks: ").split()[0])
+# family_income=int(input("enter your family income: ").split()[0])
+# Attendance_percentage=int(input("enter your attendance: ").split()[0])
+# if 18<=student_age<=25 and marks>=85 and Attendance_percentage>=75 and family_income<=300000:
+#      print("scholarship approved")
+# elif 25<student_age<18:
+#      print("invalid age!!")
+# elif marks<85:
+#      print("Reason:marks below 85")
+# elif family_income>300000:
+#      print("Reason: income above than 300000")
+# elif Attendance_percentage<75:
+#      print("Reason: attendance below 75%")
+
+# num1=int(input("enter your first number: ").split()[0])
+# num2=int(input("enter your second number: ").split()[0])
+# num3=int(input("enter your third number: ").split()[0])
+# if num1<num2<num3:
+#      print(f"second largest number is:{num2}")
+# elif num2<num1<num3:
+#      print(f"second largest number is: {num1}")
+# elif num1<num3<num2:
+#      print(f"second largest number is: {num3}")
+
