6d7d2f7eb8b91902f22c4c80ca766410fb6d51b7
sanjana-singh-cg <sanjana.singh.cg@gmail.com>
2026-09-10 09:38:15 +0530

    loop

--- python01/Loop/class_loop.py
+++ python01/Loop/class_loop.py
@@ -1,1 +1,17 @@
+#loop
 
+for i in range(1,20):
+    if i%2==0:
+        print(f"{i} is even!!")
+
+number=int(input("enter your number: ").split()[0])
+for i in range(1,number+1):
+    if i%2==1:
+        print(f"{i} is odd!!")
+
+num1=int(input("enter your first number: ").split()[0])
+num2=int(input("enter your second number: ").split()[0])
+for i in range(num1,num2+1):
+    if i%2==0:
+        print(f"{i}is even number!!")
+        
--- python01/Loop/loop.py
+++ python01/Loop/loop.py
@@ -1,1 +1,21 @@
+#assignment
+for i in range(1,6):
+    print("Hello")
+
+for i in range(0,10):
+    print(i ,end=" ")
+
+for i in range(1,11):
+    print(i)
+
+for i in range(10,1):
+    print(i)
+
+for i in range(2,20):
+    if i%2==0:
+        print(f"{i} is even number")
+
+for i in range(1,19):
+    if i%2==1:
+        print(f"{i} is odd number")
 
